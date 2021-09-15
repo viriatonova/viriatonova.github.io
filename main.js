88925fbb9fa1b66c0b19f7d5fc7e328ec863b1dc
ViriatoNova <viriatosampaio@gmail.com>
2021-09-15 15:28:40 -0300

    work on imc

--- main.js
+++ main.js
@@ -14,54 +14,55 @@ function main () {
         return p
         }
 
-        // add anos 
-        const ano = new Date();
-        const idade = ano.getFullYear() - 1982;
+    // add anos 
+    const ano = new Date();
+    const idade = ano.getFullYear() - 1982;
 
-        let anodev = ano.getFullYear() - 2021
+    let anodev = ano.getFullYear() - 2021
 
-        if ( anodev <= 0 ) {
-            anodev = 2021;
-        } else {
-            anodev = 2021 + anodev;  
-        }
-        
-        const frase1 = `Sou soteropolitano, tenho ${idade} anos e resido em Palmeiras-Ba.
-        Tenho formação em engenharia civil e trabalho com desenvolvimento web
-        desde o início de ${anodev}.`;
-        const presentSobre = document.querySelector('#present');
-        const p1 = createP();
-        p1.innerHTML = frase1;
-        presentSobre.appendChild(p1);
-
+    if ( anodev <= 0 ) {
+        anodev = 2021;
+    } else {
+        anodev = 2021 + anodev;  
+    }
     
-        /** 
-        * Capitura o evento padrão do formulário 
-        */
-        const form = document.querySelector('#imcform');
+    const frase1 = `Sou soteropolitano, tenho ${idade} anos e resido em Palmeiras-Ba.
+    Tenho formação em engenharia civil e trabalho com desenvolvimento web
+    desde o início de ${anodev}.`;
+    const presentSobre = document.querySelector('#present');
+    const p1 = createP();
+    p1.innerHTML = frase1;
+    presentSobre.appendChild(p1);
 
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            const inputAltura = event.target.querySelector('#altura');
-            const inputPeso = event.target.querySelector('#peso');
-            const altura = Number(inputAltura.value);
-            const peso = Number(inputPeso.value);
 
-            if (!altura) {
-                    setResult('Valor da Altura Invalido');
-                    return;
-            }
+    /** 
+    * Capitura o evento padrão do formulário 
+    */
+    const form = document.querySelector('#imcform');
 
-            if (!peso) {
-                    setResult('Valor do Peso Invalido');
-                    return;
-            }
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const inputAltura = event.target.querySelector('#altura');
+        const inputPeso = event.target.querySelector('#peso');
+        const altura = Number(inputAltura.value);
+        const peso = Number(inputPeso.value);
 
-            const imc = getImc(peso, altura);
-            setResult(imc);
-            setTipResult(imc);
-        
-        });
+        if (!altura) {
+                setResult('Valor da Altura Invalido');
+                return;
+        }
+
+        if (!peso) {
+                setResult('Valor do Peso Invalido');
+                return;
+        }
+
+        const imc = getImc(peso, altura);
+        setResult(imc);
+        setTipResult(imc);
+        console.log(setResult(imc));
+    
+    });
 
     function getImc(peso, altura) {
         const imc = peso / (altura**2);
@@ -71,16 +72,16 @@ function main () {
     function setResult (msg, isValid) {
         const result = document.querySelector('#result');
         result.innerHTML = '';
-        const p = document.createElement('p');
+        const p2 = createP();
         if (isValid) {
-            p.classList.add('text-red-500', 'text-lg', );
-            p.innerHTML = msg;
-            result.appendChild(p); 
+            p2.classList.add('text-red-500', 'text-xl', );
+            p2.innerHTML = msg;
+            result.appendChild(p2); 
             return;
         } else {
-            p.classList.add('text-green-500', 'text-lg', );
-            p.innerHTML = msg;
-            result.appendChild(p);
+            p2.classList.add('text-green-500', 'text-xl', );
+            p2.innerHTML = msg;
+            result.appendChild(p2);
         } 
         
     }
