69b53482211a861091af385691e7d5e17dcb4e04
viriatonova <viriatosampaio@gmail.com>
2021-12-14 11:02:34 -0300

    #add react e tailwind

---
+++ app/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <main>
+            <h1 className="blogh2">Hello World!</h1>
+        </main>
+    );
+}
+export default Home
