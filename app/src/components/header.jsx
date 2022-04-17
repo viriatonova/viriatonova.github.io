738a31944e00d154542bdfb161d77cd863bec03f
viriatonova <viriatosampaio@gmail.com>
2022-04-17 16:15:15 -0300

    add blog name

--- app/src/components/header.jsx
+++ app/src/components/header.jsx
@@ -1,13 +1,19 @@
-export default function Navbar () {
+import { useState } from 'react';
+import { useRouter } from "next/router";
+
+export default function Navbar ({ router }) {
+    const { asPath } = useRouter()
+    const name = asPath.match(/[A-Za-z]/g);
+
     return (
         <nav className="vt-nav">
             <div className="menu-left">
-                <h1 className="title-nav">Blog.</h1>
+                <h1 className="title-nav">{name}</h1>
             </div>
             <ul className="nav-ul">
-                <li><a className="nav-link"><span>Home</span></a></li>
-                <li><a className="nav-link"><span>Portifólio</span></a></li>
-                <li><a className="nav-link"><span>Small-Apps</span></a></li>
+                <li><a href="/" className="nav-link"><span>Home</span></a></li>
+                <li><a href="/portifolio" className="nav-link"><span>Portifólio</span></a></li>
+                <li><a href="/smallapps" className="nav-link"><span>Small-Apps</span></a></li>
             </ul>
         </nav>
     )
---
+++ app/src/pages/smallapps.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/header';
+import Footer from '../components/footer';
+
+const Smallapps = () => {
+    return (
+        <main className="home-wraper">
+            <Navbar />
+            <h1 className="w-full h-full flex justify-center items-center">smallapps</h1>
+            <Footer />
+        </main>
+    )
+}
+
+export default Smallapps
