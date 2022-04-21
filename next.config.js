4aa9b048a6119cf4c567d0cba6648f0b0ea60832
viriatonova <viriatosampaio@gmail.com>
2022-04-21 11:28:27 -0300

    edit url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
-    assetPrefix: isProd ? '/personalpage' : '',
+    assetPrefix: isProd ? 'https://viriatonova.github.io/' : '',
     reactStrictMode: true,
     images: {
         loader: "custom"
