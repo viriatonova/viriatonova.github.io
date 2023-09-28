28cc9aa56972079c47926c99cb4d94186836fb2c
viriato sampaio <viriatosampaio@gmail.com>
2023-09-28 16:42:59 -0300

    add next

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='w-screen h-screen'>
+      <div className='w-full h-full flex justify-center items-center'>
+        <h1 className='text-5xl'>Hello World</h1>
+      </div>
+    </main>
+  )
+}
