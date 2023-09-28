2c842b5c6a45971716204e80492860982912c460
viriato sampaio <viriatosampaio@gmail.com>
2023-09-28 16:48:53 -0300

    send deploy

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='w-screen h-screen'>
-      <div className='w-full h-full flex justify-center items-center'>
-        <h1 className='text-5xl'>Hello World</h1>
+      <div className='w-full h-full flex flex-col justify-center items-center'>
+        <h1 className='text-5xl py-4'>Hello World</h1>
+        <h2 className='text-4xl'>Deployed</h2>
       </div>
     </main>
   )
