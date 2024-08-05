f4370d1d35341ef1076e27bc8bc5edb58b5ebd4c
viriato sampaio <viriatosampaio@capaolab.com.br>
2024-08-05 08:59:27 -0300

    fix: svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/viriatonova.github.io' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: vitePreprocess()
