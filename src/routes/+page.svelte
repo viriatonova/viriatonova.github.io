e2c3cacdd3aeea5b188ef43338a9fb78340fc7b6
viriato sampaio <viriatosampaio@capaolab.com.br>
2024-08-05 08:51:55 -0300

    task: implemente svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import "../app.css"
+  import { base } from '$app/paths';
+</script>
+
+<main class="w-full h-screen flex flex-col justify-center items-center">
+  <h1>Viriato Nova</h1>
+</main>
