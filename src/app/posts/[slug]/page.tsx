1b0aa2dd5775db4ce7965b17f7f75537148e08df
viriatonova <viriatonova@gmail.com>
2026-04-16 20:51:58 -0300

    fix: client static params

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,9 +1,15 @@
-"use client"
-
 import { Post } from "@/components/Post";
 import { Container, Anchor, Group } from "@mantine/core";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { PostsHandler } from "@/lib/postsHandler";
+
+export async function generateStaticParams() {
+  const handler = new PostsHandler();
+  return handler.getAllPosts().map((post) => ({
+    slug: post.slug,
+  }));
+}
 
 export default async function Page({
   params,
@@ -18,9 +24,11 @@ export default async function Page({
     return (
       <Container size="lg" pb="xl">
         <Group mb="xl">
-          <Anchor component={Link} href="/" c="dimmed">
-            ← Voltar para a página inicial
-          </Anchor>
+          <Link href="/" passHref legacyBehavior>
+            <Anchor c="dimmed">
+              ← Voltar para a página inicial
+            </Anchor>
+          </Link>
         </Group>
         <Post
           title={metadata?.title || "Sem título"}
