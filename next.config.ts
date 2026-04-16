18ed0476eb2a4742bfcb361c6103f5d1b4c2e638
viriatonova <viriatonova@gmail.com>
2026-04-16 20:26:39 -0300

    ci: implement deploy action

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactCompiler: true,
 };
